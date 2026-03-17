ffd38612bec828e18786f6db2d7429a58ef6d782
Gabriel Araujo da Silva Brito <rm558802@fiap.com.br>
2026-03-16 21:49:37 -0300

    Criação da tela de Login (LoginScreen) com layout inicial e botão de entrada

--- app/src/main/java/com/gabaraujo08/checkpointum/MainActivity.kt
+++ app/src/main/java/com/gabaraujo08/checkpointum/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CheckpointUmTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   
+
                 }
             }
         }
